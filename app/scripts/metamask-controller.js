a651445cd86a622779e0b7c850d6aa68c9898a60
Zachary Belford <belfordz66@gmail.com>
2023-11-02 13:38:23 -0700

    Update app/scripts/metamask-controller.js

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2564,7 +2564,6 @@ export default class MetamaskController extends EventEmitter {
         return this.networkController.setProviderType(type);
       },
       setActiveNetwork: (networkConfigurationId) => {
-        // shouldnt be required now that we are using the latest sel net ctrl.
         this.selectedNetworkController.setNetworkClientIdForMetamask(
           networkConfigurationId,
         );
